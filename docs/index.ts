7a56cf3b71182c7da1ff02943738d37290f4afbd
davidgma <35199772+davidgma@users.noreply.github.com>
2023-04-18 22:04:25 +0100

    1 binaries uploaded

--- docs/index.ts
+++ docs/index.ts
@@ -4,5 +4,5 @@ onload = (event) => {
   console.log('page loaded');
   const elements = document.getElementsByClassName('myClass');
   const element = elements[0];
-  element.innerHTML = 'Hello from TypeScript (this hello message is generated programatically from index.ts)';
+  element.innerHTML = 'Hello from TypeScript (this hello message is generated programatically from index.ts).';
 };
