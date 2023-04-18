b4ad4a46d9636e308516fd68183de281c46aaad9
davidgma <35199772+davidgma@users.noreply.github.com>
2023-04-18 16:55:55 +0100

    2 files modified, 1 deleted

--- docs/javascript/index.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello from TypeScript');
-onload = function (event) {
-    console.log('page loaded');
-    var elements = document.getElementsByClassName('myClass');
-    var element = elements[0];
-    element.innerHTML = 'Hello from TypeScript';
-};
